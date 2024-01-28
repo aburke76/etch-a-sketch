b9f85dd5606d8dcb884666106aef6bb1ca13b732
aburke76 <alexlburke8771@gmail.com>
2024-01-27 20:55:47 -0500

    updated

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 const board = document.querySelector(".board");
 
+function changeSize(num) {
+    for (let i = 0; i < num; i++) {
+        for (let j = 0; j < num - 1; j++) {
+            let grid = document.createElement("div");
+            grid.className = "box";
+            board.appendChild(grid);
+        }
+        let grid = document.createElement("div");
+        grid.className = "box";
+        board.appendChild(grid);
+    }
+}
+
 function black() {}
 
 function rainbow() {}
 
 function erase() {}
 
-function changeSize(size) {}
-
 function reset() {}
+
+changeSize(16);
